2ba2557d2b48d8782c7a031ce855f4593ab098ff
envincebal <vincentyan1@gmail.com>
2020-01-28 00:03:32 -0800

    Added chat layout interface and messages state.

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,59 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import React, {Component} from "react";
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, Platform } from "react-native";
+import {GiftedChat, Bubble} from "react-native-gifted-chat";
+import KeyboardSpacer from "react-native-keyboard-spacer";
 
 export default class Chat extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      messages: []
+    }
+  }
+
+  componentDidMount(){
+    const {name} = this.props.navigation.state.params;
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+         },
+         {
+          _id: 2,
+          text: "Hello " + name + "! You have entered the chat.",
+          createdAt: new Date(),
+          system: true,
+         },
+      ]
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+  renderBubble = (props) => {
+    return (
+      <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000"
+        }
+      }} 
+      />
+    )
+  }
 
   static navigationOptions = ({navigation}) => {
     return {
@@ -11,8 +63,16 @@ export default class Chat extends Component {
 
   render(){
     return (
-    <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: this.props.navigation.state.params.color}}>
-      <Text></Text>
+    <View style={{flex:1, justifyContent: "center", backgroundColor: this.props.navigation.state.params.color}}>
+      <GiftedChat
+        renderBubble={this.renderBubble}
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      /> 
+      {Platform.OS === "android" ? <KeyboardSpacer /> : null } 
     </View>
     );
   }
--- components/Start.js
+++ components/Start.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, ImageBackground, TouchableOpacity  } from 'react-native';
 
-
 export default class Start extends Component {
   constructor(props){
     super(props);
