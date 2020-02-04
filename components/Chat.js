d45253d5c2ee98e2d3a59b0e095f21e614631b38
envincebal <vincentyan1@gmail.com>
2020-02-03 21:13:41 -0800

    Added offline storage.

--- components/Chat.js
+++ components/Chat.js
@@ -1,21 +1,25 @@
 import React, {Component} from "react";
-import { StyleSheet, Text, View, TextInput, Button, ScrollView, Platform } from "react-native";
-import {GiftedChat, Bubble} from "react-native-gifted-chat";
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, Platform, AsyncStorage, YellowBox } from "react-native";
+import NetInfo from "@react-native-community/netinfo";
+import {GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
 import KeyboardSpacer from "react-native-keyboard-spacer";
-const firebase = require('firebase');
-require('firebase/firestore');
+const firebase = require("firebase");
+require("firebase/firestore");
 
 export default class Chat extends Component {
   constructor(props){
     super(props);
-    firebase.initializeApp({
-      apiKey: "AIzaSyByZOCLQcMciuiV4nUUYH6OEAetuKBP24k",
-      authDomain: "test-7dadc.firebaseapp.com",
-      databaseURL: "https://test-7dadc.firebaseio.com",
-      projectId: "test-7dadc",
-      storageBucket: "test-7dadc.appspot.com",
-      messagingSenderId: "900068761325"
-    });
+    YellowBox.ignoreWarnings(["Setting a timer"]);
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: "AIzaSyByZOCLQcMciuiV4nUUYH6OEAetuKBP24k",
+        authDomain: "test-7dadc.firebaseapp.com",
+        databaseURL: "https://test-7dadc.firebaseio.com",
+        projectId: "test-7dadc",
+        storageBucket: "test-7dadc.appspot.com",
+        messagingSenderId: "900068761325"
+      });
+    }
 
     this.state = {
       messages: [],
@@ -24,37 +28,21 @@ export default class Chat extends Component {
         name: "",
         avatar: ""
       },
-    }
+      isConnected: false
+    } 
   }
 
-  componentDidMount(){
-    const {name} = this.props.navigation.state.params;
-
-    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async user => {
-      if (!user) {
-        await firebase.auth().signInAnonymously();
-      }
-
-      this.referenceMessages = firebase.firestore().collection("messages");   
-       
+  getMessages = async () => {
+    let messages = "";
+    try {
+      messages = (await AsyncStorage.getItem("messages")) || [];
       this.setState({
-        user: {
-          _id: user.uid,
-          name,
-          avatar: "https://placeimg.com/140/140/any"
-        },
-        loggedInText: "Hello there!"
+        messages: JSON.parse(messages)
       });
-
-      this.unsubscribeMessageUser = this.referenceMessages.onSnapshot(this.onCollectionUpdate);
-    }); 
-  }
-
-  componentWillUnmount(){
-    this.authUnsubscribe();
-    this.unsubscribeMessageUser();
-  }
-
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   addMessage() {
     let message = this.state.messages[0];
@@ -66,6 +54,14 @@ export default class Chat extends Component {
       createdAt: message.createdAt
     });
   }
+
+  deleteMessages = async () => {
+    try {
+      await AsyncStorage.removeItem("messages");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   onCollectionUpdate = (querySnapshot) => {
     const messages = [];
@@ -82,7 +78,54 @@ export default class Chat extends Component {
     this.setState({
       messages
     });
- 
+  };
+
+  componentDidMount(){
+    const {name} = this.props.navigation.state.params;
+
+    NetInfo.fetch().then(connection => {
+      if (connection.isConnected) {
+        this.setState({
+          isConnected: true
+        });
+
+        this.authUnsubscribe = firebase.auth().onAuthStateChanged(async user => {
+          if (!user) {
+            await firebase.auth().signInAnonymously();
+          }
+          
+          this.setState({
+            user: {
+              _id: user.uid,
+              name,
+              avatar: "https://placeimg.com/140/140/any"
+            }
+          });
+          
+          this.referenceMessages = firebase.firestore().collection("messages");  
+          this.unsubscribeMessageUser = this.referenceMessages.onSnapshot(this.onCollectionUpdate);
+        }); 
+        console.log("online");
+      } else {
+        this.getMessages();
+        console.log("offline");
+      }
+    });
+  }
+
+  componentWillUnmount(){
+    this.authUnsubscribe();
+    this.unsubscribeMessageUser();
+  }
+
+
+  saveMessages = async () => {
+    try {
+      await AsyncStorage.setItem("messages", JSON.stringify(this.state.messages)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   onSend(messages = []) {
@@ -90,7 +133,19 @@ export default class Chat extends Component {
       messages: GiftedChat.append(previousState.messages, messages),
     }), () => {
       this.addMessage();
+      this.saveMessages();
     });
+  }
+
+  renderInputToolbar = (props) => {
+    if (!this.state.isConnected) {
+    } else {
+      return( 
+        <InputToolbar
+        {...props}
+        />
+      );
+    }
   }
 
   renderBubble = (props) => {
@@ -120,10 +175,12 @@ export default class Chat extends Component {
     <View style={{flex:1, justifyContent: "center", backgroundColor: this.props.navigation.state.params.color}}>
       <GiftedChat
         renderBubble={this.renderBubble}
+        renderInputToolbar={this.renderInputToolbar}
         messages={this.state.messages}
         user={this.state.user}
         onSend={messages => this.onSend(messages)}
       /> 
+ 
       {Platform.OS === "android" ? <KeyboardSpacer /> : null } 
     </View>
     );
